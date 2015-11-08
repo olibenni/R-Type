f43659a3f16e6f18c6bd67c2ddba9ab95105c215
duc tran <kristjantran@yahoo.com>
2015-11-08 18:22:08 +0000

    update backGround.js added tiles

--- js/backGround.js
+++ js/backGround.js
@@ -7,14 +7,69 @@ function backGround(descr) {
 
 backGround.prototype = new Entity();
 
-backGround.prototype.speed = 1;
+backGround.prototype.speed = 1.5;
 backGround.prototype.cx = 0;
 backGround.prototype.cy = 0;
 backGround.prototype.rotation = 0;
 
+backGround.prototype.tiles = [
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1],
+	[1,1,1,1,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[],
+	[],
+	[],
+	[],
+	[1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
+
+var tileWidth = 100;
+var tileHeight = 50;
+var tilePadding = 0;
+backGround.prototype.tileOffSetLeft = 0;
+var tileOffSetTop = 0;
+
+backGround.prototype.drawTiles = function(ctx) {
+		for(var i = 0; i < this.tiles.length; i++) {
+			for(var j = 0; j < this.tiles[i].length; j++) {
+				if(this.tiles[i][j] == 1) {
+					var tilesX = (j * (tileWidth + tilePadding)) + this.tileOffSetLeft;
+					var tilesY = (i * (tileHeight + tilePadding)) + tileOffSetTop;
+					ctx.beginPath();
+					ctx.strokeStyle = 'White';
+					ctx.rect(tilesX, tilesY, tileWidth, tileHeight);
+					ctx.fillStyle = 'red';
+					ctx.stroke();
+					ctx.fill();
+					ctx.closePath();
+				}
+			}
+		}
+};
+/*
+backGround.prototype.collides = function(nextX, nextY, width, height) {
+	for(var i = 0; i < this.tiles.length; i++) {
+		for(var j = 0; j < this.tiles[i].length; j++) {
+			var tilesX = (j * (tileWidth + tilePadding)) + this.tileOffSetLeft;
+			var tilesY = (i * (tileHeight + tilePadding)) + tileOffSetTop;
+			if()
+		}
+	}	
+};
+*/
 backGround.prototype.update = function(du) {
 	// move the background backward
-	this.cx -= this.speed;
+
+	if(this.tiles[0].length * tileWidth + this.tileOffSetLeft > g_canvas.width) {
+		//console.log(this.tiles[0].length * tileWidth - this.tileOffSetLeft);
+		this.tileOffSetLeft -= this.speed;
+		this.cx -= this.speed;		
+	}
+	
 
 	if(this.cx <= 0) {
 		this.cx = g_canvas.width;
@@ -24,4 +79,6 @@ backGround.prototype.update = function(du) {
 backGround.prototype.render = function(ctx) {
 	this.sprite.drawWrappedCentredAt(
 	ctx, this.cx, this.cy, this.rotation);
+	
+	this.drawTiles(ctx);
 };

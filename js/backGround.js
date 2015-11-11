ff600a230b9b9eccfac56a0ce55b5d97faa63e21
Zodte <pedro_viking@yahoo.com.br>
2015-11-11 11:46:01 +0000

    Triggers added to backGround.js

--- js/backGround.js
+++ js/backGround.js
@@ -7,10 +7,43 @@ function backGround(descr) {
 
 backGround.prototype = new Entity();
 
-backGround.prototype.speed = 1.5;
+backGround.prototype.speed = 1;
 backGround.prototype.cx = 0;
 backGround.prototype.cy = 0;
 backGround.prototype.rotation = 0;
+backGround.prototype.distance = 0;
+
+
+
+backGround.prototype.triggerCalls = {
+	enemy : function(y,amount){
+		for(var i = 0; i < 6; i++){
+			entityManager.generateEnemy({
+				cx : 600+i*20,
+				cy : y+i*20,
+				sprite : g_sprites.enemy1[0],
+				sprites : g_sprites.enemy1,
+				scale : 1
+			});
+		}
+	},
+	
+	enemy2 : function(y,amount){
+		for(var i = 0; i < 6; i++){
+			entityManager.generateEnemy2({
+				cx : 600+i*30,
+				cyStart : y,
+				sprite : g_sprites.enemy2,
+				scale : 1
+			});
+		}
+	}
+}
+backGround.prototype.curTrigger = 0;
+backGround.prototype.triggers = [
+	[250,1,200,6],
+	[300,2,300,6]
+];
 
 backGround.prototype.tilesTop = [
 	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -73,16 +106,36 @@ backGround.prototype.collides = function() {
 
 backGround.prototype.update = function(du) {
 	// move the background backward
+	
+	this.checkTrigger();
 	if(this.tilesTop[0].length * this.tileWidth + this.tileOffSetLeft > g_canvas.width ||
 		this.tilesBottom[0].length * this.tileWidth + this.tileOffSetLeft > g_canvas.width) {
 		this.tileOffSetLeft -= this.speed;
-			
+		this.distance ++;	
 	}
+	
+	
 	this.cx -= this.speed;
 	if(this.cx <= 0) {
 		this.cx = g_canvas.width;
 	}
 };
+
+backGround.prototype.checkTrigger = function(){
+	
+	if(this.triggers[this.curTrigger][0] == this.distance){
+		var thisTrigger = this.triggers[this.curTrigger]
+		if(thisTrigger[1] == 1){
+			this.triggerCalls.enemy(thisTrigger[2],thisTrigger[3]);
+		}
+		else if(thisTrigger[1]== 2){
+			this.triggerCalls.enemy2(thisTrigger[2],thisTrigger[3]);
+		}
+		
+		if(this.curTrigger < this.triggers.length-1)
+		this.curTrigger += 1
+	}
+}
  
 backGround.prototype.render = function(ctx) {
 	

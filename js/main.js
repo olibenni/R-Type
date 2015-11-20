7f4452a56a80f7b692b2ef18bb19185eb4b38ef5
Kristjana Eir <kristjanaeir@Kristjanas-MacBook-Pro.local>
2015-11-20 12:01:26 +0000

    'N' to quit instead of 'Q'

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ main.gameOver = function () {
 
 // Simple voluntary quit mechanism
 //
-var KEY_QUIT = 'Q'.charCodeAt(0);
+var KEY_QUIT = 'N'.charCodeAt(0);
 function requestedQuit() {
     return keys[KEY_QUIT];
 }
